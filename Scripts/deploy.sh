71ca85fadebbe3c2c2d39990a0da0ff993a48ed3
Christopher Oaks <OAKSCA1@ETSU.EDU>
2025-04-20 21:00:29 -0400

    Refining...

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -71,7 +71,7 @@ cleanup() {
         # Create Snapshot 
         create_snapshot() {
         echo "📷Creating snapshot..."
-        curl -X POST http://3.232.16.65:8080/snapshots/create -d "description=Automated snapshot before shutdown"
+        curl -X POST http://3.232.16.65:8080/snapshots/create -d "description=Automated snapshot before shutdown" 2>/dev/null
         }
 
         create_snapshot
