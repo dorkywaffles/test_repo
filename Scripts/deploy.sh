9b5b587dc70cbc44e835dcfdf2b9d0e121744184
Christopher Oaks <OAKSCA1@ETSU.EDU>
2025-04-20 20:46:42 -0400

    bug fixes :hearteyes:

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -71,7 +71,7 @@ cleanup() {
         # Create Snapshot 
         create_snapshot() {
         echo "Creating snapshot..."
-        curl -X POST http://3.232.16.65:8080//snapshots/create -d "description=Automated snapshot before shutdown"
+        curl -X POST http://3.232.16.65:8080/snapshots/create -d "description=Automated snapshot before shutdown"
         }
 
         create_snapshot
--- Scripts/shutdown.sh
+++ Scripts/shutdown.sh
@@ -2,7 +2,7 @@
 
 create_snapshot() {
     echo "Creating snapshot..."
-    curl -X POST http://3.232.16.65:8080//snapshots/create -d "description=Automated snapshot before shutdown"
+    curl -X POST http://3.232.16.65:8080/snapshots/create -d "description=Automated snapshot before shutdown"
 }
 
 create_snapshot
